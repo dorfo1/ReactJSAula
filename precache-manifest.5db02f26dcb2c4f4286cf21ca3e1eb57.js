2ebc7b49b89610674f94b917a9ff863f568d5b36
Rodolfo <rodolfo__BN@hotmail.com>
2019-06-16 18:52:55 -0300

    Updates

---
+++ precache-manifest.5db02f26dcb2c4f4286cf21ca3e1eb57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96c245ba0f0d44dd578dda7779837896",
+    "url": "/ReactJSAula/index.html"
+  },
+  {
+    "revision": "4ba6d75a1cf9c544eacc",
+    "url": "/ReactJSAula/static/css/main.a10d1592.chunk.css"
+  },
+  {
+    "revision": "b025720c1f310c360ab2",
+    "url": "/ReactJSAula/static/js/2.4954678c.chunk.js"
+  },
+  {
+    "revision": "4ba6d75a1cf9c544eacc",
+    "url": "/ReactJSAula/static/js/main.702727b1.chunk.js"
+  },
+  {
+    "revision": "62c6767170880f58e245",
+    "url": "/ReactJSAula/static/js/runtime~main.61a2cade.js"
+  }
+]);
